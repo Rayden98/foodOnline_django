2ae76c9f18e97d1cc2829a3d493a3a3deaa240b9
Enmanuel Cruz <rember98cjs@gmail.com>
2024-04-25 22:36:59 -0600

    orders and paypal implementation

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -34,7 +34,10 @@ urlpatterns = [
     path('search/', MarketplaceViews.search, name='search'),
     
     # CHECKOUT
-    path('checkout/', MarketplaceViews.checkout, name="checkout")
+    path('checkout/', MarketplaceViews.checkout, name="checkout"),
+    
+    # ORDERS
+    path('orders/', include('orders.urls')),
     
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
+from accounts.models import UserProfile
+
 from .context_processors import get_cart_amounts, get_cart_counter
 from menu.models import Category, FoodItem
 from vendor.models import OpeningHour, Vendor
@@ -166,15 +168,29 @@ def search(request):
         }    
         
         return render(request, 'marketplace/listings.html', context)
+ 
+   
     
-    
+@login_required(login_url='login')
 def checkout(request):
     cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
     cart_count = cart_items.count()
     if cart_count <= 0:
         return redirect('marketplace')
     
-    form = OrderForm()
+    user_profile = UserProfile.objects.get(user=request.user)
+    default_values = {
+        'first_name': request.user.first_name,
+        'last_name': request.user.last_name,
+        'phone': request.user.phone_number,
+        'email': request.user.email,
+        'address': user_profile.address,
+        'country': user_profile.country,
+        'state': user_profile.state,
+        'city': user_profile.city,
+        'pin_code': user_profile.pin_code,
+    }
+    form = OrderForm(initial=default_values)
     context = {
         'form': form,
         'cart_items': cart_items,    
